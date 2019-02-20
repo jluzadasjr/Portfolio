1288de15fca65d6770d130c7b604f1546692e87c
jluzadasjr <jayrl707@Jesuss-MacBook-Pro.local>
2019-02-19 18:40:27 -0800

    updated code

---
+++ js/init.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  M.updateTextFields();
+});
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
